0dfb79d7a5a01dd862cd8f4a1de24ef0f41efa68
João Victor <jvpinheirodossantos@gmail.com>
2023-02-17 13:32:33 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -8,7 +8,6 @@ import java.util.Set;
 
 import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
